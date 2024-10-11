7ae6cb3d9c83d759ccb87b9837c49984768fdda7
aayush-kalia <147748535+aayush-kalia@users.noreply.github.com>
2024-10-11 11:59:22 +0530

    updated profile

--- src/profile.js
+++ src/profile.js
@@ -1,41 +1,39 @@
-function showAddAddressModal() {
-  const modal = document.getElementById("addAddressModal");
-  modal.style.display = "block";
-  isFormVisible = true;
-}
-
-function closeAddAddressModal() {
-  const modal = document.getElementById("addAddressModal");
-  modal.style.display = "none";
-  isFormVisible = false;
-}
-
 function showProfileDetails() {
   hideAllSections();
   document.getElementById("profileDetails").style.display = "block";
   setActiveLink(0);
 }
 
-function showOrderDetails() {
+function showMentorDetails() {
   hideAllSections();
-  document.getElementById("orderDetails").style.display = "block";
+  document.getElementById("mentorDetails").style.display = "block";
   setActiveLink(1);
 }
 
-function showAddressBook() {
+function showCourses() {
   hideAllSections();
-  document.getElementById("addressBook").style.display = "block";
+  document.getElementById("coursesSelected").style.display = "block";
   setActiveLink(2);
 }
 
+function showActivityLog() {
+  hideAllSections();
+  document.getElementById("activityLog").style.display = "block";
+  setActiveLink(3);
+}
+
 function hideAllSections() {
-  document.getElementById("orderDetails").style.display = "none";
   document.getElementById("profileDetails").style.display = "none";
-  document.getElementById("addressBook").style.display = "none";
+  document.getElementById("mentorDetails").style.display = "none";
+  document.getElementById("coursesSelected").style.display = "none";
+  document.getElementById("activityLog").style.display = "none";
 }
 
 function setActiveLink(index) {
-  document.querySelector(".list-group-item.active").classList.remove("active");
+  const activeItem = document.querySelector(".list-group-item.active");
+  if (activeItem) {
+    activeItem.classList.remove("active");
+  }
   document.querySelectorAll(".list-group-item")[index].classList.add("active");
 }
 
